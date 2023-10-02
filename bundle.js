d845a0de657caaf96f6b922abc048d40a09d8243
BTW-AAK <aditamitkumar@gmail.com>
2023-10-02 22:54:03 +0530

    Added front end for output and translate page.

--- bundle.js
+++ bundle.js
@@ -1,3 +1,4 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 
 
 var generateButton = document.querySelector('#generateButton');
@@ -7,7 +8,7 @@ var ccButton = document.querySelector('#ccButton');
 if (generateButton){
 
   
-  const { TextServiceClient } = require("@google-ai/generativelanguage/").v1beta2;
+  const { TextServiceClient } = ("@google-ai/generativelanguage").v1beta2;
   const { read } = ("fs");
   const { GoogleAuth } = ("google-auth-library");
   const MODEL_NAME = "models/text-bison-001";
@@ -54,3 +55,5 @@ if (ccButton){
     navigator.clipboard.writeText(text);
     console.log('Content copied to clipboard');
   })}
+
+},{}]},{},[1]);
